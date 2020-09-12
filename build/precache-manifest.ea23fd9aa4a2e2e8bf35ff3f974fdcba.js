273f03967033da0fc029e72799b481bf3faadf58
Polina Rudenko <rudenkopolina@gmail.com>
2020-09-13 01:41:30 +0300

    add build folder

---
+++ build/precache-manifest.ea23fd9aa4a2e2e8bf35ff3f974fdcba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722a706bda9c9da1ba9f0a22fc0d47e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67b930dce4c43569ae37",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a7c6dfaf0dbf7402ad1",
+    "url": "/static/js/2.b8e8ef50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b8e8ef50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b930dce4c43569ae37",
+    "url": "/static/js/main.cfe18ca3.chunk.js"
+  },
+  {
+    "revision": "26f73ebc0f5108126450",
+    "url": "/static/js/runtime-main.f09c01d9.js"
+  },
+  {
+    "revision": "29be6f310bccc015e2e308db43453b1c",
+    "url": "/static/media/avatar.29be6f31.jpeg"
+  }
+]);
