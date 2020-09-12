9947cf10215fa20c749d85a94611b9fdb1797282
Polina Rudenko <rudenkopolina@gmail.com>
2020-09-13 01:49:39 +0300

    Updates

---
+++ precache-manifest.d01415b04bc1231d583a2fff1f45e281.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f493fa4b098dc062839c829ce06cfe0f",
+    "url": "/cv_polina_rudzenka/index.html"
+  },
+  {
+    "revision": "33bbea5caba6fb6cbb47",
+    "url": "/cv_polina_rudzenka/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de0d205d99ba19985fce",
+    "url": "/cv_polina_rudzenka/static/js/2.dde4d5d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cv_polina_rudzenka/static/js/2.dde4d5d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33bbea5caba6fb6cbb47",
+    "url": "/cv_polina_rudzenka/static/js/main.8dc407b2.chunk.js"
+  },
+  {
+    "revision": "d4167ae06a3de842fd0e",
+    "url": "/cv_polina_rudzenka/static/js/runtime-main.21f58de4.js"
+  },
+  {
+    "revision": "29be6f310bccc015e2e308db43453b1c",
+    "url": "/cv_polina_rudzenka/static/media/avatar.29be6f31.jpeg"
+  }
+]);
